5c26ff11c56454ca56cf67712dddab5b698c8afe
= <loveneverfails95@naver.com>
2022-11-11 17:31:11 +0900

    david final

--- my-vue-project/src/components/MovieCard.vue
+++ my-vue-project/src/components/MovieCard.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="col-4 card h-25">
     <div class="h-25">
-      <img :src="`https://image.tmdb.org/t/p/original/${result.poster_path}`" class="card-img-top" height="500">
+      <img :src="`https://image.tmdb.org/t/p/original/${result?.poster_path}`" class="card-img-top" height="500">
     </div>
     <div class="card-body">
-      <h5 class="card-title">{{result.original_title}}</h5>
-      <p class="card-text">{{result.overview}}</p>
+      <h5 class="card-title">{{result?.original_title}}</h5>
+      <p class="card-text">{{result?.overview}}</p>
     </div>
   </div>
 </template>
